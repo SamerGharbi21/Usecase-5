5e7fbcf033e8989421a4c99071195ea7559f0a02
Samer <CS.Samer21@gmail.com>
2024-08-12 00:48:39 +0300

    12

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 import base64
 
@@ -8,14 +9,15 @@ st.set_page_config(
 
 st.write("<h1 style='text-align: right;'>🤔تدور على وظيفه ؟<h1><br>", unsafe_allow_html=True)
 
-# Load the GIF and encode it to base64
-gif_path = "/image.gif"
+gif_path = "C:\\Users\\Public\\Desktop\\Usecase-5\\image.gif"
 
-with open(gif_path, "rb") as gif_file:
-    gif_bytes = gif_file.read()
-    encoded_gif = base64.b64encode(gif_bytes).decode("utf-8")
+# Check if the file exists
+if os.path.exists(gif_path):
+    with open(gif_path, "rb") as gif_file:
+        gif_bytes = gif_file.read()
+        encoded_gif = base64.b64encode(gif_bytes).decode("utf-8")
 
-# Embed the GIF in the app
-gif_html = f'<img src="data:image/gif;base64,{encoded_gif}" alt="Your Awesome GIF">'
-
-st.write(gif_html, unsafe_allow_html=True)
+    gif_html = f'<img src="data:image/gif;base64,{encoded_gif}" alt="Your Awesome GIF">'
+    st.write(gif_html, unsafe_allow_html=True)
+else:
+    st.error("GIF file not found. Please check the file path.")
