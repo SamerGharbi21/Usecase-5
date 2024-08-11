a525eb54bf4de071cdd6a148c673f3c8ad12f556
Samer <CS.Samer21@gmail.com>
2024-08-12 00:51:47 +0300

    15

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,11 @@ with open(gif_path, "rb") as gif_file:
     gif_bytes = gif_file.read()
     encoded_gif = base64.b64encode(gif_bytes).decode("utf-8")
 
-# Embed the GIF in the app
-gif_html = f'<img src="data:image/gif;base64,{encoded_gif}" alt="Your Awesome GIF">'
+# Embed the GIF in the app and align it to the center
+gif_html = f'''
+<div style="text-align: center;">
+    <img src="data:image/gif;base64,{encoded_gif}" alt="Your Awesome GIF">
+</div>
+'''
 
 st.write(gif_html, unsafe_allow_html=True)
