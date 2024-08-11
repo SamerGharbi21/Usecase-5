10115efa1fb68677be817f8b144528bf28ec851f
Samer <CS.Samer21@gmail.com>
2024-08-12 00:42:41 +0300

    8

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import base64
 
 st.set_page_config(
     page_title="Hello",
@@ -9,3 +10,13 @@ st.write("<h1 style='text-align: right; '>🤔تدور على وظيفه ؟ <h1>
 
 st.write("![Your Awsome GIF](C://Users//Public//Desktop//Usecase-5//image.gif)")
 
+"""### gif from local file"""
+file_ = open("C://Users//Public//Desktop//Usecase-5//image.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+
+st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,
+)
