bc6afed017181e8d42aea81b416750cf899dd324
Samer <CS.Samer21@gmail.com>
2024-08-12 01:08:27 +0300

    25

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.write(gif_html, unsafe_allow_html=True)
 st.write("<p style='text-align: right;'>انا طالب مثل اي طالب الحمدلله بشهاده جامعية و اطمح بوظيفه براتب عالي , طبعا سمعت كلام من ناس كثير ان اغلب الوظايف في الرياض و لازم اخرج من البلد عشان الاقي الرواتب العاليه و السلم الوظيفي الممتاز فا قررت ابحث عن هاذي المعلومات<p>", unsafe_allow_html=True)
 st.write("<p style='text-align: right;'>طبعا انا من سكان المدينة المنورة فا قررت ابحث عن مدى توفر الوظايف بمدينتي <p>", unsafe_allow_html=True)
 
-st.image("image1.png" , width=800)
+st.image("image1.png" , width=750)
 
 st.write("<p style='text-align: right;'><p>", unsafe_allow_html=True)
 st.write("<p style='text-align: right;'><p>", unsafe_allow_html=True)
